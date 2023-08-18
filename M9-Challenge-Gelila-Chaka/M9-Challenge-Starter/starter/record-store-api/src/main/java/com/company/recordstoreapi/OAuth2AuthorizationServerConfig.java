8c4af634ba0647e43ce9cf1a35e5fb8a2e4075f5
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2023-08-18 06:10:35 +0300

    files

---
+++ M9-Challenge-Gelila-Chaka/M9-Challenge-Starter/starter/record-store-api/src/main/java/com/company/recordstoreapi/OAuth2AuthorizationServerConfig.java
@@ -0,0 +1,52 @@
+package com.company.recordstoreapi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) {
+        security.checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("client")
+                .secret("$2a$10$7cKDAKLQ3BG.A9A5lWVG1.NvKv1TJ2GhYY.WOQq0kIJOPABkxB.k2") // Client secret: password
+                .authorizedGrantTypes("password")
+                .scopes("read", "write")
+                .resourceIds("record-store-api");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.authenticationManager(authenticationManager)
+                .tokenStore(tokenStore())
+                .userDetailsService(userDetailsService);
+    }
+}
---
+++ M9-Challenge-Gelila-Chaka/M9-Challenge-Starter/starter/record-store-api/src/main/java/com/company/recordstoreapi/RecordStoreApiApplication.java
@@ -0,0 +1,12 @@
+package com.company.recordstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordStoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecordStoreApiApplication.class, args);
+    }
+}
